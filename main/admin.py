f8bb2d0a87b2c149e72de9fd0852b5e33cfa3ef7
MNTYROS <mnadzimtahara@gmail.com>
2024-09-17 22:19:26 +0700

    Tugas 2 w/ readme

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+adminName = os.environ.get("ADMIN_NAME")
+adminEmail = os.environ.get("ADMIN_EMAIL")
+adminPassword = os.environ.get("ADMIN_PASSWORD")
+
+def create_superuser(username, email, password):
+    if User.objects.filter(username=username).exists():
+        print(f"Username {username} already exists.")
+    else:
+        User.objects.create_superuser(username, email, password)
+        print(f"Superuser {username} created successfully.")
+
+create_superuser(adminName, adminEmail, adminPassword)
+
+admin.site.register(Product)
--- main/models.py
+++ main/models.py
@@ -1,14 +1,13 @@
 from django.db import models
+import uuid
 
 class Product(models.Model):
+    id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name=models.CharField(max_length=255, default="car")
     price=models.IntegerField(default="0")
     description=models.TextField(default="a car")
-    nama=models.CharField(max_length=255, default="mahasiswa")
-    npm=models.IntegerField(default="0")
-    kelas=models.CharField(max_length=255, default="PBP")
 
-# class Myself(models.Model):
-#     nama=models.CharField(max_length=255)
-#     npm=models.IntegerField()
-#     kelas=models.CharField(max_length=255)
+class Me(models.Model):
+    nama=models.CharField(max_length=255, default="Muhammad Nadzim Tahara")
+    npm=models.CharField(max_length=255, default="2306275430")
+    kelas=models.CharField(max_length=255, default="PBP C 2024")
